3191b3947057bf8dda94976aa38a7c30c8945d53
LuisHernandez1999 <105570863+LuisHernandez1999@users.noreply.github.com>
2024-07-31 00:43:58 -0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ NUMERO_WHATSAPP = ''
 MENSAGEM_INICIAL = "Oi, sou Baby Joe! Como está sua vibe hoje? Triste, feliz, reflexiva ou animada?"
 
 
-YOUTUBE_API_KEY = 'AIzaSyD-JQtXfLdr4judsVevJW3t5ijpPTV_2qA'
+YOUTUBE_API_KEY = ''
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
